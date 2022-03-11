87e8996277638857530f13f33b8e7b676e810db5
Robert Quartey <37972474+robertquartey7@users.noreply.github.com>
2022-03-10 19:23:47 -0500

    .

--- js-files/script.js
+++ js-files/script.js
@@ -1,6 +1,12 @@
 const dateTime = document.getElementById('dateTime');
+const toggleButton = document.querySelector('.toggle-button');
+const menu = document.querySelector('.menu');
+
+
 
 const date = new Date()
-
-
 dateTime.innerHTML = date.getFullYear()
+
+toggleButton.addEventListener('click', function(){
+    menu.classList.toggle('show')
+})
